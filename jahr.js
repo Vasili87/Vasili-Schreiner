6e5ea2aa3478c373ee2095b8566a8424cc454c94
Vasili Schreiner <Wasja1987@gmx.de>
2024-11-16 17:24:14 +0100

    Portfolio mit über Git verbunden

---
+++ jahr.js
@@ -0,0 +1,17 @@
+document.getElementById("jahr").textContent = new Date().getFullYear();
+
+// Funktion zum Anzeigen des Buttons, wenn gescrollt wird
+window.onscroll = function() {
+    var button = document.getElementById("nachOben");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+// Funktion, um nach oben zu scrollen
+function nachObenFunktion() {
+    document.body.scrollTop = 0; // Für Safari
+    document.documentElement.scrollTop = 0; // Für Chrome, Firefox, IE und Opera
+}
